e74ac7dd46bb44a5e74c4a1867f31e26b419ea3f
b1ruk4 <birukwondimubkk@gmail.com>
2022-04-07 14:57:58 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[20] = "and that piece of art is useful" - Dora Korpar, 2015-10-19,\n";
+	write(str[20]);
+	return (0);
+}
