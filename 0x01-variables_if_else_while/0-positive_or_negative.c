79078c6eb24b9c853bad48c9db3e55bf8709910f
b1ruk4 <birukwondimubkk@gmail.com>
2022-04-10 10:48:17 -0700

    fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,7 @@
  *
  *return: Always 0.
  */
-int main(void)
+int main(void) //return type
 {
 int n;
 
@@ -22,7 +22,7 @@ else if (n < 0)
 printf("%d is negative\n", n);
 else
 
-printf("%d is zero", n);
+printf("%d is zero\n", n);
 
 return (0);
 }
