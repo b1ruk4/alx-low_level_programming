5152ee1f120bdf6724d7cc852dfc1e3dfb2ed618
b1ruk4 <birukwondimubkk@gmail.com>
2022-04-16 13:50:08 -0700

    task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+void positive_or_negative(int i);
+int larger_number(int a, int b, int c);
+void print_remaning_days(int month, int dat, int year);
+int convert_day(int month, int day);
+#endif
