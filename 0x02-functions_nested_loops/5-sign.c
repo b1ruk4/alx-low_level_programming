ce89c5092ce9ceab98099f048d53be4390237528
b1ruk4 <birukwondimubkk@gmail.com>
2022-04-16 12:25:00 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,13 +4,13 @@
  * @n: The char to be checked.
  *
  * Return: 1 if char is  greaterthan zero and print +
- * return 0 and print 0 if it is equal to zero 
+ * return 0 and print 0 if it is equal to zero
  * rerutn -1 if n is equal to less than zero and print -.
  */
 int print_sign(int n)
 {
         if (n > '0')
-	{	
+	{
         	_putchar('+');
 		return (1);
 	}
