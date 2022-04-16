5788b79e2a4a85e443200bf169f456e7ee12fcd3
b1ruk4 <birukwondimubkk@gmail.com>
2022-04-16 12:16:42 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,13 +11,13 @@ int print_sign(int n)
 {
         if (((n > '0') ||
         (n = '0') || (n < '0')))
-                printf("+");
+                _putchar('+');
 		return (1);
         else if
-		printf("0");
+		_putchar('0');
 		return (0);
 	else
-                printf("-");
+                _putchar('-');
 		return (-1);
 }
 
