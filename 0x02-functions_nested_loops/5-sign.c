09a7108fed8c3c48853938bc937eae317bdb1576
b1ruk4 <birukwondimubkk@gmail.com>
2022-04-16 12:13:57 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - Checks if a character is alphabetic.
+ * @n: The char to be checked.
+ *
+ * Return: 1 if char is  greaterthan zero and print +
+ * return 0 and print 0 if it is equal to zero 
+ * rerutn -1 if n is equal to less than zero and print -.
+ */
+int print_sign(int n)
+{
+        if (((n > '0') ||
+        (n = '0') || (n < '0')))
+                printf("+");
+		return (1);
+        else if
+		printf("0");
+		return (0);
+	else
+                printf("-");
+		return (-1);
+}
+
