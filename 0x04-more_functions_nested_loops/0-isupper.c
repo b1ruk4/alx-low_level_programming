e59a1d2076470b59d13d1b5eea026b98b3f45e70
b1ruk4 <birukwondimubkk@gmail.com>
2022-04-19 11:37:51 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,14 +7,17 @@
  *Return:1 - uppercase,0 - otherwise
  *
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
 }
