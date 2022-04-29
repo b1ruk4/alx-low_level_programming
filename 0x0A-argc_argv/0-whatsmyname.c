873060ef21122208db7a594523df5ecc03ea2525
b1ruk4 <birukwondimubkk@gmail.com>
2022-04-29 13:39:17 -0700

    whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,8 @@
 
 /**
  * main - print the name  of the program
- *
+ *@argc: sie of argv
+ *@argv: arry jof the pointer 
  * return: is 0.
  */
 int main(int argc __attribute__((unused)), char *argv[])
