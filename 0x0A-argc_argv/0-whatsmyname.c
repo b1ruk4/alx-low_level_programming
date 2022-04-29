f0dafdd902b3a10dab02b6275b7183c5b421109f
b1ruk4 <birukwondimubkk@gmail.com>
2022-04-29 13:20:44 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the name  of the program
+ *
+ * return: is 0.
+ */
+int main (int argc, char *argv[])
+{
+	pritnf("%s\n", argv[0]);
+	return (0);
+}
