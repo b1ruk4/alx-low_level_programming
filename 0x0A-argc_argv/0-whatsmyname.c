43f641c0543ecba08374e1da58f43ce992657991
b1ruk4 <birukwondimubkk@gmail.com>
2022-04-29 13:37:07 -0700

    whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
  *
  * return: is 0.
  */
-int main (int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	pritnf("%s\n", argv[0]);
+	
+	printf("%s\n", argv[0]);
 	return (0);
 }
