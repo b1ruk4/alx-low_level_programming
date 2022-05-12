b2e26e668e6d3c874c7dde22e3b6dfac8ffb61fd
b1ruk4 <birukwondimubkk@gmail.com>
2022-05-12 09:23:00 -0700

    task #1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
-#ifndef _function_pointers_h_
-#define _function_pointers_h_
+#ifndef function_pointers_h
+#define function_pointers_h
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
 
