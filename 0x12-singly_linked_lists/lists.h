5154bb37495e51ba877eb869ab196759f9814e41
b1ruk4 <birukwondimubkk@gmail.com>
2022-05-19 09:57:24 -0700

    fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,19 +5,6 @@
 * Desc: header file containinf prototypes and definitions
 *	for all functions and types written in this proj
 */
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 #include <stdlib.h>
 
@@ -42,5 +29,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
+int _putchar(char c);
 #endif
