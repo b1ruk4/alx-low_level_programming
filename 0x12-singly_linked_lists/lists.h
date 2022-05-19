0c07c34ad6240797f7b4a41b26918ca1bd06174b
b1ruk4 <birukwondimubkk@gmail.com>
2022-05-19 09:55:02 -0700

    fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,19 @@
 * Desc: header file containinf prototypes and definitions
 *	for all functions and types written in this proj
 */
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 #include <stdlib.h>
 
